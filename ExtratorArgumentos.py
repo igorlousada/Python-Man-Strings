1bcf96cb55223e3b43c507b9ffd233e6b988bf5e
Igor Lousada <igorcoutinho97@gmail.com>
2020-10-16 12:46:24 -0300

    adicionando novos métodos

--- ExtratorArgumentos.py
+++ ExtratorArgumentos.py
@@ -1,7 +1,7 @@
 class ExtratorArgumentosUrl:
     def __init__(self,url):
         if(self.url_valida(url)):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("Url Invalida")    
 
@@ -16,10 +16,36 @@ class ExtratorArgumentosUrl:
     def __str__(self):
         return "objeto criado"
 
-    def ExtraiArgumentos(self):
+    def extrai_argumentos(self):
+        moeda_origem = 'moedaorigem='
+        moeda_destino = 'moedadestino='
+        indiceInicialMoedaOrigem = self.busca_moeda(moeda_origem)
+        indiceFinalMoedaOrigem =  self.url.find('&')
+        indiceFinalMoedaDestino = self.url.find('&',indiceFinalMoedaOrigem + 1)
+        indiceInicialMoedaDestino = self.busca_moeda(moeda_destino)
+        moedaOrigem = self.url[indiceInicialMoedaOrigem:indiceFinalMoedaOrigem]
+        moedaDestino = self.url[indiceInicialMoedaDestino:indiceFinalMoedaDestino]
+        if(moedaOrigem == "moedadestino"):
+            self.troca_moeda_origem()
+            indiceInicialMoedaOrigem = self.busca_moeda(moeda_origem)
+            indiceFinalMoedaOrigem =  self.url.find('&')
+            moedaOrigem = self.url[indiceInicialMoedaOrigem:indiceFinalMoedaOrigem]    
+        valor = self.extrai_valor()
+        return moedaOrigem,moedaDestino,valor
 
-        indiceFinalMoedaDestino = 
-        indiceInicialMoedaDestino = 
+    def busca_moeda(self,moeda_buscada):
+        return  self.url.find(moeda_buscada) + len(moeda_buscada)
 
-        indiceInicialMoedaOrigem = 
-        indiceFinalMoedaOrigem =  
+    def troca_moeda_origem(self):
+        self.url = self.url.replace("moedadestino","real",1)  
+        print(self.url)
+        return None
+    def extrai_valor(self):
+        busca_valor = 'valor='
+        indice_inicial_valor = self.busca_moeda(busca_valor)
+        valor = self.url[indice_inicial_valor:]
+        return valor
+        #print(valor)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ExtratorArgumentos import ExtratorArgumentosUrl
 
-url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
+url = "https://bytebank.com/cambio?moedaorigem=moedadestino&moedadestino=dolar&valor=700"
 #url = None
-print(ExtratorArgumentosUrl.url_valida(url))
+#print(ExtratorArgumentosUrl.url_valida(url))
 #print(argumento)
+valor_moeda = ExtratorArgumentosUrl(url)
+var = valor_moeda.extrai_argumentos()
+print(var[1])
